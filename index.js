b247d76368590c64a48c31d0f7a04ffcd8e809a8
Aleksandr Sokolov <asok10@yandex.ru>
2019-05-21 16:32:25 +0200

    bugfix

--- index.js
+++ index.js
@@ -5,88 +5,88 @@
  */
 export class ImageDrop {
 
-	/**
-	 * Instantiate the module given a quill instance and any options
-	 * @param {Quill} quill
-	 * @param {Object} options
-	 */
-	constructor(quill, options = {}) {
-		// save the quill reference
-		this.quill = quill;
-		this.options = options;
-		// bind handlers to this instance
-		this.handleDrop = this.handleDrop.bind(this);
-		this.handlePaste = this.handlePaste.bind(this);
-		// listen for drop and paste events
-		this.quill.root.addEventListener('drop', this.handleDrop, false);
-		this.quill.root.addEventListener('paste', this.handlePaste, false);
-	}
+    /**
+     * Instantiate the module given a quill instance and any options
+     * @param {Quill} quill
+     * @param {Object} options
+     */
+    constructor(quill, options = {}) {
+        // save the quill reference
+        this.quill = quill;
+        this.options = options;
+        // bind handlers to this instance
+        this.handleDrop = this.handleDrop.bind(this);
+        this.handlePaste = this.handlePaste.bind(this);
+        // listen for drop and paste events
+        this.quill.root.addEventListener('drop', this.handleDrop, false);
+        this.quill.root.addEventListener('paste', this.handlePaste, false);
+    }
 
-	/**
-	 * Handler for drop event to read dropped files from evt.dataTransfer
-	 * @param {Event} evt
-	 */
-	handleDrop(evt) {
-		evt.preventDefault();
-		if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length) {
-			if (document.caretRangeFromPoint) {
-				const selection = document.getSelection();
-				const range = document.caretRangeFromPoint(evt.clientX, evt.clientY);
-				if (selection && range) {
-					selection.setBaseAndExtent(range.startContainer, range.startOffset, range.startContainer, range.startOffset);
-				}
-			}
-			this.readFiles(evt.dataTransfer.files, this.insert.bind(this));
-		}
-	}
+    /**
+     * Handler for drop event to read dropped files from evt.dataTransfer
+     * @param {Event} evt
+     */
+    handleDrop(evt) {
+        evt.preventDefault();
+        if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length) {
+            if (document.caretRangeFromPoint) {
+                const selection = document.getSelection();
+                const range = document.caretRangeFromPoint(evt.clientX, evt.clientY);
+                if (selection && range) {
+                    selection.setBaseAndExtent(range.startContainer, range.startOffset, range.startContainer, range.startOffset);
+                }
+            }
+            this.readFiles(evt.dataTransfer.files, this.insert.bind(this));
+        }
+    }
 
-	/**
-	 * Handler for paste event to read pasted files from evt.clipboardData
-	 * @param {Event} evt
-	 */
-	handlePaste(evt) {
-		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
-			this.readFiles(evt.clipboardData.items, dataUrl => {
-				const selection = this.quill.getSelection();
-				if (selection) {
-					// we must be in a browser that supports pasting (like Firefox)
-					// so it has already been placed into the editor
-				}
-				else {
-					// otherwise we wait until after the paste when this.quill.getSelection()
-					// will return a valid index
-					setTimeout(() => this.insert(dataUrl), 0);
-				}
-			});
-		}
-	}
+    /**
+     * Handler for paste event to read pasted files from evt.clipboardData
+     * @param {Event} evt
+     */
+    handlePaste(evt) {
+        if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length === 1) {
+            this.readFiles(evt.clipboardData.items, dataUrl => {
+                const selection = this.quill.getSelection();
+                if (selection) {
+                    // we must be in a browser that supports pasting (like Firefox)
+                    // so it has already been placed into the editor
+                }
+                else {
+                    // otherwise we wait until after the paste when this.quill.getSelection()
+                    // will return a valid index
+                    setTimeout(() => this.insert(dataUrl), 0);
+                }
+            });
+        }
+    }
 
-	/**
-	 * Insert the image into the document at the current cursor position
-	 * @param {String} dataUrl  The base64-encoded image URI
-	 */
-	insert(dataUrl) {
-		const index = (this.quill.getSelection() || {}).index || this.quill.getLength();
-		this.quill.insertEmbed(index, 'image', dataUrl, 'user');
-	}
+    /**
+     * Insert the image into the document at the current cursor position
+     * @param {String} dataUrl  The base64-encoded image URI
+     */
+    insert(dataUrl) {
+        const index = (this.quill.getSelection() || {}).index || this.quill.getLength();
+        this.quill.insertEmbed(index, 'image', dataUrl, 'user');
+    }
 
-	/**
-	 * Extract image URIs a list of files from evt.dataTransfer or evt.clipboardData
-	 * @param {File[]} files  One or more File objects
-	 * @param {Function} callback  A function to send each data URI to
-	 */
-	readFiles(files, callback) {
-		// check each file for an image
-		[].forEach.call(files, file => {
-			if (!file.type.match(/^image\/(gif|jpe?g|a?png|svg|webp|bmp|vnd\.microsoft\.icon)/i)) {
-				// file is not an image
-				// Note that some file formats such as psd start with image/* but are not readable
-				return;
-			}
-			//check if the function was passed in options for handle img else past base64
-			if(this.options.handler && typeof this.options.handler === "function"){
+    /**
+     * Extract image URIs a list of files from evt.dataTransfer or evt.clipboardData
+     * @param {File[]} files  One or more File objects
+     * @param {Function} callback  A function to send each data URI to
+     */
+    readFiles(files, callback) {
+        // check each file for an image
+        [].forEach.call(files, file => {
+            if (!file.type.match(/^image\/(gif|jpe?g|a?png|svg|webp|bmp|vnd\.microsoft\.icon)/i)) {
+                // file is not an image
+                // Note that some file formats such as psd start with image/* but are not readable
+                return;
+            }
+            //check if the function was passed in options for handle img else past base64
+            if (this.options.handler && typeof this.options.handler === "function") {
                 this.options.handler(file, callback);
-			}else {
+            } else {
                 // set up file reader
                 const reader = new FileReader();
                 reader.onload = (evt) => {
@@ -98,7 +98,7 @@ export class ImageDrop {
                     reader.readAsDataURL(blob);
                 }
             }
-		});
-	}
+        });
+    }
 
 }
